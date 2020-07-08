0735b2d7e5dc886209d58ba6329490abac1fc2f2
Thatcooldevguy <tsommers150@gmail.com>
2020-07-08 18:25:06 -0400

    Debug

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, StatusBar} from 'react-native';
+
+const instructions = Platform.select({
+  // eslint-disable-next-line prettier/prettier
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#4F6D7A" />
+        <Text style={styles.welcome}>SunBurn Debug Mode</Text>
+        <Text style={styles.instructions}>1.0.0</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4F6D7A',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#F5FCFF',
+    marginBottom: 5,
+  },
+});
